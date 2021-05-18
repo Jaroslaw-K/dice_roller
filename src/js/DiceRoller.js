39490a4a7fd29d5ed0436f909fad833da89b6427
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2021-05-18 15:12:07 +0200

    added form with choose number option

--- src/js/DiceRoller.js
+++ src/js/DiceRoller.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
-
 import '../scss/diceRoller.scss';
-// import D6 from './dices/d6';
-// import D4 from './dices/d4';
 
 class DiceRoller extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dices: [],
+            number: undefined,
+            windowAction: false,
         };
     };
 
     roll = () => {
         let allDices = this.state.dices.map(dice => {return {type: dice.type, result: Math.floor(Math.random() * (dice.type)) + 1}});   
         this.setState({
-            dices: allDices
+            dices: allDices,
+            windowAction: false
         },()=> console.log(this.state.dices));
     }
 
@@ -23,8 +23,8 @@ class DiceRoller extends Component {
         var sum = 0;
         this.state.dices.forEach(dice => sum += dice.result);
         var element = (<React.Fragment>
-            <p>{this.state.dices.map((dice, index) => dice.result?<span key={index}>{`D${dice.type}`}:{dice.result}&nbsp;</span>:"")}</p>
-            <p>{sum > 0?`SUM:${sum}`:""}</p>
+            <p className="rollContainer">{this.state.dices.map((dice, index) => dice.result?<div className="singleRoll" key={index}>{`D${dice.type}`}:{dice.result}&nbsp;</div>:"")}</p>
+            <p className="sum">{sum > 0?`SUM:${sum}`:""}</p>
         </React.Fragment>);
             
         return element; 
@@ -32,7 +32,8 @@ class DiceRoller extends Component {
 
     clear = () => {
         this.setState({
-            dices: []
+            dices: [],
+            windowAction: false,
         })
     };
 
@@ -42,25 +43,57 @@ class DiceRoller extends Component {
         diceTab.push({type: dice, result: null});
 
         this.setState({
-            dices: diceTab
-        }, ()=> console.log(this.state.dices));      
+            dices: diceTab,
+            number: ""
+        }, ()=> console.log(this.state.dices));  
+        
     };
 
     choosedDices = () => {
         let countDices = {};
         this.state.dices.forEach(dice => {  
-            if(countDices[`d${dice.type}`] === undefined ){
-                countDices[`d${dice.type}`] = 0;
+            if(countDices[`D${dice.type}`] === undefined ){
+                countDices[`D${dice.type}`] = 0;
             }
-            countDices[`d${dice.type}`] +=1;
+            countDices[`D${dice.type}`] +=1;
         });
         let elements = [];
         for (const [key, value] of Object.entries(countDices)) {
-            if (value == 0) continue;  
-            elements.push(<span key={key}>{`${key}:${value}`}&nbsp;</span>)  
+            if (value === 0) continue;  
+            elements.push(<div className="choosedDice" key={key}>{`${key}:${value}`}</div>)  
           }
+          console.log('work')
           return elements;
     };
+
+    handleNumberChange = (e) => {
+        this.setState({number: e.target.value});
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if (parseFloat(this.state.number) <= 1 || parseFloat(this.state.number) >9999 || this.state.number === undefined) {
+            this.setState({
+                windowAction: true
+            });
+        } else {
+            this.setState({windowAction: false});
+        }
+    };
+
+    openChooseWindow = () => {
+        this.setState({windowAction: true});
+    }
+
+    createForm = () => {
+        let form;
+        form = <form className="form" onSubmit={this.handleSubmit}>
+                <label>PLEASE CHOOSE NUMBER BETWEEN 2 AND 9999</label>
+                <input className="number" type="number" name="number" value={this.state.number} onChange={this.handleNumberChange}/>
+                <input onClick={() => this.addDice(parseFloat(this.state.number))} type="submit" value="Accept"/>
+            </form>;
+            return form;
+    }
 
     render () {
         return ( 
@@ -68,24 +101,25 @@ class DiceRoller extends Component {
                 <section className="section sectionTop">
                     <div className="resultContainer">
                     {this.show()}
+                    {this.state.windowAction?this.createForm():""}
                     </div>
                     <div className="displayContainer">
-                            {this.choosedDices()}
+                        {this.choosedDices()}
                     </div>
                 </section>
                 <section className="section sectionBottom">
                     <div className="dices"> 
-                        <div className="diceBox" onClick={() => this.addDice(4)}>D4</div>
-                        <div className="diceBox" onClick={() => this.addDice(6)}>D6</div>
-                        <div className="diceBox" onClick={() => this.addDice(8)}>D8</div>
-                        <div className="diceBox" onClick={() => this.addDice(10)}>D10</div>
-                        <div className="diceBox" onClick={() => this.addDice(12)}>D12</div>
-                        <div className="diceBox" onClick={() => this.addDice(20)}>D20</div>
-                        <div className="diceBox" onClick={() => this.addDice(parseInt(window.prompt("Choose any dice number")))}>Choose Dice</div>
+                        <button className="btn btnD4" onClick={() => this.addDice(4)}>D4</button>
+                        <button className="btn btnD6" onClick={() => this.addDice(6)}>D6</button>
+                        <button className="btn btnD8" onClick={() => this.addDice(8)}>D8</button>
+                        <button className="btn btnD10" onClick={() => this.addDice(10)}>D10</button>
+                        <button className="btn btnD12" onClick={() => this.addDice(12)}>D12</button>
+                        <button className="btn btnD20" onClick={() => this.addDice(20)}>D20</button>
+                        <button className="btn btnChoose" onClick={()=>this.openChooseWindow()}>CHOOSE DICE</button>
                     </div>
                     <div className="buttons">
-                        <button type="submit" className="btn btnRoll" onClick={() => this.roll()}>ROLL</button>
-                        <button type="submit" className="btn btnClear" onClick={() => this.clear()}>CLEAR</button>
+                        <button type="submit" className="btn" onClick={() => this.roll()}>ROLL</button>
+                        <button type="submit" className="btn" onClick={() => this.clear()}>CLEAR</button>
                     </div>
                 </section>
             </main>
